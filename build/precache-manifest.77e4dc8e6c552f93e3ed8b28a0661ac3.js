65ee8be422bb91f6fa478eb71040ab66dfa7cc9b
Sven Schmidt <0xsven@gmail.com>
2019-10-22 17:49:50 +0200

    adding public and build folder

---
+++ build/precache-manifest.77e4dc8e6c552f93e3ed8b28a0661ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddd66eea4b1037040199",
+    "url": "/static/css/main.5a6fca07.chunk.css"
+  },
+  {
+    "revision": "ddd66eea4b1037040199",
+    "url": "/static/js/main.ddd66eea.chunk.js"
+  },
+  {
+    "revision": "832de53d40ea42f2a102",
+    "url": "/static/css/1.deb01e4a.chunk.css"
+  },
+  {
+    "revision": "832de53d40ea42f2a102",
+    "url": "/static/js/1.832de53d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83ceea7eba1370f9560dadee84054872",
+    "url": "/index.html"
+  }
+];
